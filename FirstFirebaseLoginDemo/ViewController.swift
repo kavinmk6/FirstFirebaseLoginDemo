ae453045c5e5c99189e409ba789bef0a0e053f5c
kavin <kavin.mohandass@ibm.com>
2021-11-01 00:48:19 -0300

    First Phase of FirebaseAuthUI Authentication

--- FirstFirebaseLoginDemo/ViewController.swift
+++ FirstFirebaseLoginDemo/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuthUI
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func loginAction(_ sender: UIButton) {
+        
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+            
+            return
+        }
+        
+        authUI?.delegate = self
+        
+        let authUiViewController = authUI!.authViewController()
+        
+        present(authUiViewController, animated: true, completion: nil)
 
+    }
+    
+    
+}
+
+extension ViewController: FUIAuthDelegate{
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+       
+        if error != nil{
+            return
+        }
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
 }
 
