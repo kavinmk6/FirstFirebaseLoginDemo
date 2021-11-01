c692b29589ff7f094263d63037af7c560472e1f2
kavin <kavin.mohandass@ibm.com>
2021-10-31 22:18:14 -0300

    Initial Commit

---
+++ FirstFirebaseLoginDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstFirebaseLoginDemo
+//
+//  Created by Kavin prabu Mohandass on 2021-10-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
